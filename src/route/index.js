76656536230698ad75234bac53028ab47bc23816
Sergii <52467090+SergAdmin@users.noreply.github.com>
2023-08-22 08:17:47 +0300

    user

--- src/route/index.js
+++ src/route/index.js
@@ -1,7 +1,61 @@
 // Підключаємо технологію express для back-end сервера
 const express = require('express')
+const { TRUE } = require('sass')
 // Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
 const router = express.Router()
+
+// ================================================================
+
+class User {
+  static #list = []
+
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  verifyPassword = (password) => this.password === password
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => this.#list
+
+  static getById = (id) =>
+    this.#list.find((user) => user.id === id)
+  // this.#list.findIndex((user) => user.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+  static updateById = (id, { email }) => {
+    const user = this.getById(id)
+
+    if (user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
+  }
+}
 
 // ================================================================
 
@@ -11,12 +65,80 @@ const router = express.Router()
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
+  const list = User.getList()
+
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('user-index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
+    style: 'user-index',
+
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+router.post('/user-create', function (req, res) {
+  const { email, login, password } = req.body
+
+  const user = new User(email, login, password)
+
+  User.add(user)
+
+  console.log(User.getList())
+
+  // console.log(req.body)
+  res.render('user-success-info', {
+    style: 'user-success-info',
+    info: 'Користувача створено',
+  })
+})
+
+// ================================================================
+
+router.get('/user-delete', function (req, res) {
+  const { id } = req.query
+
+  // console.log(typeof id)
+
+  User.deleteById(Number(id))
+
+  res.render('user-success-info', {
+    style: 'user-success-info',
+    info: 'Користувача видалено',
+  })
+})
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+
+  let result = false
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  console.log(email, password, id)
+
+  // const result = true // User.updateById(Number(id), { email })
+
+  res.render('user-success-info', {
+    style: 'user-success-info',
+    info: result
+      ? 'Пошта користувача оновлена'
+      : 'Сталася помилка',
+  })
 })
 
 // ================================================================
